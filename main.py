9212cb80f0866c0a94336ba37e4cc30fbb2fd08a
newmen1234 <mail@newmen.gmbh>
2025-06-05 15:08:01 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import openai
 import os
@@ -17,3 +19,8 @@ async def chat(data: Prompt):
         messages=[{"role": "user", "content": data.prompt}]
     )
     return {"response": response.choices[0].message["content"]}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("index.html", "r") as f:
+        return f.read()
